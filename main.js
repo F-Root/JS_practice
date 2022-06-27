5a8a0c9af784fc77b80a16437a7fc4ec61d42cd4
RooT_Kim <bdd3452@gmail.com>
2022-06-27 10:53:27 +0900

    Conditional Statements

--- main.js
+++ main.js
@@ -2,12 +2,29 @@
 //HTML은 웹브라우저에 한번 출력되면 자기자신을 바꿀 수 없지만 JS를 이용하면 변경이 가능하다.
 //JS는 웹페이지를 동적으로 만들어준다.
 
-// Data type
-
+// -- Data type --
 // Primitives : Number, String, Boolean, Null, Undefined, Symbol(es6) => No Reference (원시형)
 // Object, Function => Reference (참조형)
 
 // variable : var, let
 // constant : const
+
+// = : 대입연산자 (assignment operators)
+
+// ==, === : 비교연산자 (comparison operators) => return Boolean (true, false)
+
+// Conditional Statements : 조건문
+/*
+  if(boolean ? true : false)
+    ~
+  else
+    ~
+*/
+/*
+  switch(data type)
+    case type1 : ~
+    case type2 : ~
+    default: ~
+*/
 
 function main() {}
