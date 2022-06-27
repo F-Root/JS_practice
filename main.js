adbec0aef4ff7d5163558c35ec34e2000da5ef89
RooT_Kim <bdd3452@gmail.com>
2022-06-27 09:26:30 +0900

    JS day / night mode

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+//JS는 웹브라우저에서 사용자와 상호작용을 하기 위해 개발됨
+//HTML은 웹브라우저에 한번 출력되면 자기자신을 바꿀 수 없지만 JS를 이용하면 변경이 가능하다.
+//JS는 웹페이지를 동적으로 만들어준다.
+
+// Data type
+
+// Primitives : Number, String, Boolean, Null, Undefined, Symbol(es6) => No Reference (원시형)
+// Object, Function => Reference (참조형)
+
+// variable : var, let
+// constant : const
+
 function main() {}
