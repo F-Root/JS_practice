d085c8fe1e4897eecb33fbefcdbf555f78785c48
RooT_Kim <bdd3452@gmail.com>
2022-06-30 04:51:30 +0900

    JS class done

--- class.js
+++ class.js
@@ -2,24 +2,41 @@
 // constructor function의 대체재 => es6에서 추가.
 
 class Person {
-  // class 내에서는 함수를 선언할때 function키워드를 붙이지 않음.(규칙)
+  // class 내에서는 함수(메소드)를 선언할때 function키워드를 붙이지 않음.(규칙)
   // 그리고 생성자 함수를 선언할때는 무조건 constructor를 붙여야함.(규칙)
   // 그 외 구현방식은 constructor function(생성자 함수)과 비슷한다.
   constructor(name, first, second) {
     this.name = name;
     this.first = first;
     this.second = second;
-    console.log('constructor'); // constructor 함수와 똑같이 new 연산자를 통해 객체 인스턴스가 생성될때 호출됨.
+    console.log('class'); // constructor 함수와 똑같이 new 연산자를 통해 객체 인스턴스가 생성될때 호출됨.
+  }
+
+  // JS class내에서 메소드를 선언하는 방식. prototype에 사용하는 것보다 권장됨.
+  // class내에서 선언한 메소드는 prototype에 명시되어 있음. (객체 내에 메소드로 할당되는 것이 아닌 애초부터 prototype에 할당됨.)
+  sum() {
+    return 'in class : ' + (this.first + this.second);
   }
 }
 
-var kim = new Person('kim', 10, 20); // console.log('constructor'); 출력
+// class를 사용할때도 prototype에 메소드를 추가할 수 있다. 허나 권장방식은 아님.
+Person.prototype.mul = function () {
+  return 'prototype : ' + this.first * this.second;
+};
+
+var kim = new Person('kim', 10, 20); // console.log('class'); 출력
 
 console.log(Person);
 console.log(kim);
-// kim.sum = function () {
-//   return 'this : ' + (this.first + this.second);
-// };
-// var lee = new Person('lee', 10, 10);
-// console.log(kim.sum());
-// console.log(lee.sum());
+console.log(kim.sum());
+console.log(kim.mul());
+
+//마찬가지로 중간에 다른방식으로 동작하는 메소드를 선언하고 싶으면 아래와 같이 할 수 있다.
+kim.sum = function () {
+  return 'this : ' + (this.first + this.second);
+};
+
+var lee = new Person('lee', 10, 10); // console.log('class'); 출력
+
+console.log(kim.sum());
+console.log(lee.sum());
