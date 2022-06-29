a451dff4584d615c97d92c50d686033531384f05
RooT_Kim <bdd3452@gmail.com>
2022-06-29 09:59:30 +0900

    JS prototype start

---
+++ object.js
@@ -0,0 +1,107 @@
+/* this => ('나는', '저는' 과 같은 역할) */
+var kim = {
+  name: 'kim',
+  first: 10,
+  second: 20,
+  // sum: function (f, s) {
+  //   return f + s; => 동작O but 이미 객체 내에 명시되어있는 property를 매개변수로 넣어주어야 하는 번거로움이 있음.
+  // },
+  // sum: function() {
+  //   return first + second; => 동작X undefined가 뜸
+  // }
+  // sum: function () {
+  //   return kim.first + kim.second; => 동작O but 유연하지가 않음. 객체 이름을 k라고 바꾸면 kim.first는 없는 데이터가 됨. k.first로 바꿔야함.
+  // },
+  sum: function () {
+    return this.first + this.second;
+  },
+};
+
+// console.log(kim.sum(kim.first, kim.second));
+console.log(kim.sum());
+
+/* constructor (object factory) -생성자- */
+var lee = {
+  name: 'lee',
+  first: 10,
+  second: 10,
+  sum: function () {
+    return this.first + this.second;
+  },
+};
+
+console.log(lee.sum());
+
+//example of object factory (Date obj)
+var date = new Date('2022-06-27');
+console.log(date.getFullYear());
+console.log(date.getMonth()); //0부터 counting 되기 때문에 1을 더해줘야 한다.
+console.log(date.getDate());
+
+console.log('build-in obj : Date', Date);
+
+// function은 object이고, constructor를 만들 수 있다. => this를 사용하는 순간 constructor가 되는 듯 하다.
+function Person() {
+  this.name = 'kim';
+  this.first = 10;
+  this.second = 20;
+  this.sum = function () {
+    return this.first + this.second;
+  };
+  console.log('hello'); // => this가 있으면 출력되지 않음
+}
+
+//생성자 함수(constructor)는 리턴이 없기 때문에 아무것도 출력되지 않는다.
+console.log('Person()', Person());
+
+//new keyword를 붙이면 object를 생성하는 constructor(생성자)가 된다.
+console.log('new Person()', new Person()); // => 생성자 함수.
+
+//parameter를 받는 생성자 함수 (constructor)
+function Person(name, first, second) {
+  this.name = name;
+  this.first = first;
+  this.second = second;
+  this.sum = function () {
+    return this.first + this.second;
+  };
+}
+
+//새로운 객체 생성
+var kim = new Person('kim', 10, 20); //함수 호출 앞에 new 키워드를 사용하면 그 함수는 '생성자 함수'라고 불리며 결과로 객체가 생성됨.
+var lee = new Person('lee', 10, 10);
+console.log(kim);
+console.log(kim.sum());
+console.log(lee.sum());
+
+//Math는 객체
+console.log(Math);
+
+//생성자는 함수로 출력
+console.log(Person);
+console.log(Date);
+
+//일반 함수는 생성자 함수와 똑같이 함수로 출력
+function test() {
+  return 'hello';
+}
+console.log(test);
+
+//생성자 함수와 일반 함수 테스트
+function normalTest() {
+  this.test = 'normal';
+  return 'normal';
+}
+
+console.log(normalTest);
+console.log(normalTest()); //리턴 String인 'hello'가 출력이 되긴함.
+var normal = new normalTest();
+console.log(normal); // 객체 출력 (this.test = 'hello' 가 없으면 빈 객체가 출력됨.)
+
+function constructorTest() {
+  this.test = 'constructor';
+}
+
+console.log(constructorTest);
+var constructor = new constructorTest();
+console.log(constructor); // 객체 출력
---
+++ prototype.js
@@ -0,0 +1,16 @@
+/* prototype */
+// JavaScript = prototype based language
+
+function Person(name, first, second) {
+  this.name = name;
+  this.first = first;
+  this.second = second;
+  this.sum = function () {
+    return this.first + this.second;
+  };
+}
+
+var kim = new Person('kim', 10, 20);
+var lee = new Person('lee', 10, 10);
+console.log(kim.sum());
+console.log(lee.sum());
